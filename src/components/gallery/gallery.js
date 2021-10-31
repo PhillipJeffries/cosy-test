88097c0b430744d875f85902d702c3b8666d5c8e
PhillipJeffries <ggbox@mail.ru>
2021-11-01 00:20:32 +0400

    1

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -6,6 +6,8 @@ import {useDispatch, useSelector} from 'react-redux';
 
 import {useEffect, useState} from 'react';
 
+import uniqid from 'uniqid';
+
 
 
 
@@ -41,6 +43,32 @@ const Gallery = () => {
     const showPic = (e) => {
         dispatch({type: 'SHOW_SLIDER', hidden: false, index: +e.target.id+1})
     }
+
+
+    const [commentText, setCommentText] = useState('');
+    const [commentId, setCommentId] = useState('');
+
+    const onChangeComment = (e) => {
+        setCommentText(e.target.value);
+        setCommentId(e.target.id);
+        e.target.value= commentText;
+        console.log(commentText, commentId)
+    }
+
+    const changeComment = (e) => {
+        e.preventDefault();
+        console.log('changeComment')
+        dispatch({type: 'EDIT_COMMENT', id: commentId, comment: commentText})
+    }
+
+
+    useEffect(() =>{
+        if(commentText){
+            setCommentText(commentText);
+        }
+    },[commentText])
+
+
   
     return(
         <ul className="gallery-wrapper">
@@ -51,8 +79,10 @@ const Gallery = () => {
                             <div className='img-wrapper'>
                                 <img id={i} onClick={showPic} alt={el.comment} src={`${process.env.PUBLIC_URL}${el.url}`}/>
                             </div>
-                            
-                            <input  value={el.comment} className="comment" onClick={(e)=>console.log(e.target)}/>
+                            <form className="comment-form" onSubmit={changeComment}>
+                                <input id={el.id} onChange={onChangeComment}  value={el.comment} className="comment" onClick={(e)=>console.log(e.target)}/>
+                                <input type="submit" hidden/>
+                            </form>
                             
 
                         </li>
--- src/redux/reducers/galleryReducer.js
+++ src/redux/reducers/galleryReducer.js
@@ -8,9 +8,25 @@ export const galleryReducer = (state=initialState, action) => {
     switch (action.type) {
         case 'EDIT_COMMENT':
             console.log('editComment')
+            // const id = action.id;
+            // const newComment = action.comment
+            // const {oldData} = state;
+            // const itemIndex = picturesData.findIndex(res => res.id === action.id);            
             
+            const arr = [];
+            picturesData.forEach(elem =>{
+                if(elem.id===action.id){
+                    elem.comment = action.comment;
+                    arr.push(elem);
+                }
+
+            })
+
+            
+
             return{
-                ...state
+                ...state,
+                picturesData: arr
             }
         case 'ADD_PHOTO':
             console.log('addPhoto', picturesData);
