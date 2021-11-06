413df455e9599deb6a8cb2fbfc5f57995ac7346d
PhillipJeffries <ggbox@mail.ru>
2021-11-06 11:48:47 +0400

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.scss';
+import moment from 'moment';
+
 import Gallery from './components/gallery/gallery';
 import Slider from './components/slider/slider';
 import AddPhotoForm from './components/addPhotoForm/addPhotoForm';
+
+
+console.log(moment())
+
+const date = new Date();
+
+console.log(date.getFullYear())
 
 
 function App() {
